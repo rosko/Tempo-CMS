e53254398cfa024d093a6aa4f0d685d5d79318d7
Алексей Волков <a@insvit.com>
2010-12-18 16:42:01 +0200

    Удаление лишних библиотек иконок.

--- protected/config/iconsets.php
+++ protected/config/iconsets.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'iconic' => array(
+/*    'iconic' => array(
         'sizes' => array(
             '8x8', '12x12', '16x16', '24x24', '32x32'
         ),
@@ -21,7 +21,7 @@ return array(
             'files' => 'folder_stroke',
             'exit' => 'x',
         )
-    ),
+    ),*/
     'simplicio' => array(
         'sizes' => array(
             '32x32', '48x48', '64x64', '128x128'
@@ -40,86 +40,6 @@ return array(
             'sitemap' => 'document_search',
             'files' => 'folder',
             'exit' => 'notification_error',
-        )
-    ),
-    'tutorial9' => array(
-        'sizes' => array(
-            '32x32',
-        ),
-        'template' => array( // alias, size
-            'url' => '/images/icons/tutorial9/{alias}32.png',
-        ),
-        'aliases' => array(
-            'add' => 'plus',
-            'delete' => 'linedpaperminus',
-            'up' => 'uparrow',
-            'down' => 'downarrow',
-            'move' => 'exchange',
-            'edit' => 'paperpencil',
-            'settings' => 'gear',
-            'sitemap' => 'article',
-            'files' => 'folder',
-            'exit' => 'stop',
-        )
-    ),
-    'rinoa' => array(
-        'sizes' => array(
-            '24x24',
-        ),
-        'template' => array( // alias, size
-            'url' => '/images/icons/rinoa/{alias}.png',
-        ),
-        'aliases' => array(
-            //'add' => 'add',
-            //'delete' => 'delete',
-            //'up' => 'up',
-            //'down' => 'down',
-            'move' => 'go',
-            'edit' => 'document_edit',
-            'settings' => 'applications',
-            'sitemap' => 'home',
-            'files' => 'folder',
-            'exit' => 'close',
-        )
-    ),
-    'led-icons' => array(
-        'sizes' => array(
-            '16x16',
-        ),
-        'template' => array( // alias, size
-            'url' => '/images/icons/led-icons/{alias}.png',
-        ),
-        'aliases' => array(
-            //'add' => 'add',
-            //'delete' => 'delete',
-            'up' => 'arrow_up',
-            'down' => 'arrow_down',
-            'move' => 'arrow_inout',
-            'edit' => 'pencil',
-            'settings' => 'cog',
-            'sitemap' => 'application_view_list',
-            'files' => 'folder',
-            'exit' => 'cross',
-        )
-    ),
-    'drf' => array(
-        'sizes' => array(
-            '32x32',
-        ),
-        'template' => array( // alias, size
-            'url' => '/images/icons/drf/{alias}.png',
-        ),
-        'aliases' => array(
-            'add' => 'new doc', // plus
-            'delete' => 'trash',
-            'up' => 'up_alt',
-            'down' => 'down_alt',
-            'move' => 'refresh',
-            'edit' => 'edit',
-            'settings' => 'window',
-            'sitemap' => 'search',
-            'files' => 'folder',
-            'exit' => 'public',
         )
     ),
     'fatcow' => array(
